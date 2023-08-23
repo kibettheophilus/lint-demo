baba8be01fd66dbc5e69c6c5321019c4684d99e2
theophiluskibet <kibettheophilus@gmail.com>
2023-08-23 16:32:02 +0300

    add android lint dependcies and plugin

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -2,9 +2,21 @@
 plugins {
     id("java-library")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
+    id("com.android.lint")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+dependencies {
+    compileOnly("com.android.tools.lint:lint-api:31.1.0")
+    compileOnly("com.android.tools.lint:lint-checks:31.1.0")
+//    compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.20")
 }
