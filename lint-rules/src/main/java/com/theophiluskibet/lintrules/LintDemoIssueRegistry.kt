4f4f9021e4186f935f3b522c7e63180b9c563383
theophiluskibet <kibettheophilus@gmail.com>
2023-08-23 17:25:01 +0300

    register log detector issue in issue registry

--- lint-rules/src/main/java/com/theophiluskibet/lintrules/LintDemoIssueRegistry.kt
+++ lint-rules/src/main/java/com/theophiluskibet/lintrules/LintDemoIssueRegistry.kt
@@ -1,9 +1,21 @@
 package com.theophiluskibet.lintrules
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
+import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 
 class LintDemoIssueRegistry : IssueRegistry() {
-    override val issues: List<Issue>
-        get() = TODO("Not yet implemented")
+    override val issues: List<Issue> = listOf(AndroidLogDetector.ISSUE)
+
+    override val api: Int = CURRENT_API
+
+    override val minApi: Int
+        get() = 8
+
+    override val vendor: Vendor = Vendor(
+        vendorName = "Theophilus Kibet",
+        feedbackUrl = "https://github.com/kibettheophilus/lint-demo/issues",
+        contact = "kibettheophilus@gmail.com",
+    )
 }
