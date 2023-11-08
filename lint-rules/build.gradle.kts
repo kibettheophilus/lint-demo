826ca254d01672546a6417873ef8594e5317c77b
theophiluskibet <kibettheophilus@gmail.com>
2023-11-08 14:49:23 +0300

    update jvm target to 17

--- lint-rules/build.gradle.kts
+++ lint-rules/build.gradle.kts
@@ -2,22 +2,24 @@
 plugins {
     id("java-library")
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
-    id("com.android.lint")
+    alias(libs.plugins.android.lint)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 dependencies {
     compileOnly(libs.lint.api)
-    compileOnly(libs.lint.checks)
+//    compileOnly(libs.lint.checks)
     compileOnly(libs.kotlin.stdlib)
     testImplementation(libs.lint.test)
+    testImplementation(libs.junit)
+    testImplementation(libs.lint.cli)
 }
